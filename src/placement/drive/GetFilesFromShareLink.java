77fec0d332130f719569b0cd782f5e56e1e195ac
suresh <suresh301190@gmail.com>
2014-10-29 01:09:03 +0530

    GetFilesFromDriveLink complete v1.0

--- src/placement/drive/GetFilesFromShareLink.java
+++ src/placement/drive/GetFilesFromShareLink.java
@@ -1,31 +1,34 @@
 package placement.drive;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
-import java.security.GeneralSecurityException;
-import java.util.Arrays;
-
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.http.GenericUrl;
-import com.google.api.client.http.HttpResponse;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.drive.Drive;
-import com.google.api.services.drive.DriveScopes;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 
 public class GetFilesFromShareLink {
 
 	private static final String requestURL =  "https://docs.google.com/a/iiitd.ac.in/uc?export=download&id=";
 	private final String dir;
-	
+	private static final boolean DEBUG = true;
+	private static int rc = 0;
+	private static final Pattern sID1 = Pattern.compile(" *(MT)?[0-9]* *");
+	private static final Pattern sID2 = Pattern.compile("[a-zA-Z]* *- *(MT)?[0-9]*");
+
 	public GetFilesFromShareLink(String dir){
-		this.dir = dir;
+		if(dir == null || dir.length() == 0){
+			System.out.println("Please provide a Valid directory");
+			this.dir = null;
+			System.exit(-1);
+		}
+		else{
+			this.dir = dir + ((dir.charAt(dir.length() - 1) == '/' )?"":"/");
+		}
 	}
 
 	public void SaveFile(String ID, String fileName, boolean isBtech) {
@@ -33,69 +36,120 @@ public class GetFilesFromShareLink {
 			HttpURLConnection con =(HttpURLConnection) new URL(requestURL + ID).openConnection();
 			con.setRequestMethod("GET");
 			if(isBtech)
-				Files.copy(con.getInputStream(), new java.io.File(dir + fileName + ".pdf").toPath());
+				Files.copy(con.getInputStream(), new File(dir + "B.Tech/" + fileName + ".pdf").toPath());
 			else
-				Files.copy(con.getInputStream(), new java.io.File(dir + fileName + ".pdf").toPath());
+				Files.copy(con.getInputStream(), new File(dir + "M.Tech/" + fileName + ".pdf").toPath());
 
 		} catch(Exception e){
 			e.printStackTrace();
 		}
 	}
 
-	/** Email of the Service Account */
-	private static final String SERVICE_ACCOUNT_EMAIL = 
-			"292882852430-mp1l9lqkr2fl0c56rg5u0eehafqsv5k3@developer.gserviceaccount.com";
+	public static void main(String[] args){
+		if(args.length == 2){
+			System.out.println("args[0] = path/to/output/dir, args[1] = path/to/student/csv ");
+			return;
+		}
 
-	/** Path to the Service Account's Private Key file */
-	private static final String SERVICE_ACCOUNT_PKCS12_FILE_PATH = 
-			"URL-947be66e22b9.p12";
-
-	/**
-	 * Build and returns a Drive service object authorized with the service accounts.
-	 *
-	 * @return Drive service object that is ready to make requests.
-	 */
-	public static Drive getDriveService() throws GeneralSecurityException,
-	IOException, URISyntaxException {
-		HttpTransport httpTransport = new NetHttpTransport();
-		JacksonFactory jsonFactory = new JacksonFactory();
-		GoogleCredential credential = new GoogleCredential.Builder()
-		.setTransport(httpTransport)
-		.setJsonFactory(jsonFactory)
-		.setServiceAccountId(SERVICE_ACCOUNT_EMAIL)
-		.setServiceAccountScopes(Arrays.asList(DriveScopes.DRIVE))
-		.setServiceAccountPrivateKeyFromP12File(
-				new java.io.File(SERVICE_ACCOUNT_PKCS12_FILE_PATH))
-				.build();
-		Drive service = new Drive.Builder(httpTransport, jsonFactory, null)
-		.setHttpRequestInitializer(credential)
-		.setApplicationName("GetFiles").build();
-		return service;
+		ArrayList<Student> toSave = parseInput(new File(args[1]));
+	}
+	
+	public void test1(String line){
+		String[] parts = "naresh11067@iiitd.ac.in,https://docs.google.com/document/d/1plpiOzRVH-ybsA-0_oFCMfv2QpzI7HYuhnqX9AZrfTE/edit?usp=sharing,B.Tech,Btech- 2011067".split(",");
+		System.out.println(new Student(
+				parseID(parts[1])
+				, parseSID(parts[3])
+				, parts[2].equals("B.Tech")
+				, parseName(parts[0])).toString());
 	}
 
-	/**
-	 * Download a file's content.
-	 * 
-	 * @param service Drive API service instance.
-	 * @param file Drive File instance.
-	 * @return InputStream containing the file's content if successful,
-	 *         {@code null} otherwise.
-	 */
-	private static InputStream downloadFile(Drive service, String URL) {
-		if (URL != null && URL.length() > 0) {
-			try {
-				HttpResponse resp =
-						service.getRequestFactory().buildGetRequest(new GenericUrl(URL))
-						.execute();
-				return resp.getContent();
-			} catch (IOException e) {
-				// An error occurred.
-				e.printStackTrace();
-				return null;
+	private static ArrayList<Student> parseInput(File file) {
+		// TODO Auto-generated method stub
+
+		ArrayList<Student> toSave = new ArrayList<Student>();
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String line;
+			while(null != (line = br.readLine())){
+				String[] parts = line.split(",");
+				toSave.add(new Student(
+						parseID(parts[1])
+						, parseSID(parts[3])
+						, parts[2].equals("B.Tech")
+						, parseName(parts[0])));
 			}
-		} else {
-			// The file doesn't have any content stored on Drive.
-			return null;
+			br.close();
+		} catch (IOException e) {
+			if(DEBUG)
+				e.printStackTrace();
+		}
+
+		return toSave;
+	}
+
+	private static String parseName(String s) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		while(i < s.length()){
+			if(s.charAt(i) <= '9')
+				break;
+			i++;
+		}
+		return s.substring(0, i);
+	}
+
+	private static String parseSID(String s) {
+		// TODO Auto-generated method stub
+
+		String sID = "shark" + rc++;
+		try{
+			if(sID1.matcher(s).matches()){
+				sID = s.trim();
+			}
+			else if(sID2.matcher(s).matches()){
+				sID = s.substring(s.lastIndexOf('-') + 1).trim();
+			}
+		} catch(Exception e){
+			if(DEBUG)
+				e.printStackTrace();
+		}
+
+		return sID;
+	}
+
+	private static String parseID(String s) {
+		// TODO Auto-generated method stub
+
+		String ID = "shark" + rc++;
+
+		try{
+			if(s.contains("/view?usp=sharing") 
+					|| s.contains("/edit?usp=sharing") 
+					|| s.contains("/edit?usp=docslist_api")){
+				ID = s.substring(s.lastIndexOf('/', s.lastIndexOf('/') - 1) + 1, s.lastIndexOf('/'));
+			}
+		} catch(Exception e){
+
+		}
+
+		return ID;
+	}
+
+	static class Student{
+		final String fileName, ID, sID, name;
+		boolean isBtech;
+
+		public Student(String ID, String sID, boolean isBtech, String name){
+			this.sID = sID;
+			this.isBtech = isBtech;
+			this.ID = ID;
+			this.name = name;
+			this.fileName = sID + (isBtech?"_Btech_":"_Mtech_") + name;
+		}
+
+		@Override
+		public String toString(){
+			return "{" + name + (isBtech?" B.Tech":" M.Tech") + ", ID-> " + ID + ", sID-> " + sID + "}";
 		}
 	}
 }
